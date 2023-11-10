a3b1bb26bacd0584cac44e6325a61be78ae5d4d5
havr1985 <gavrtel1985@gmail.com>
2023-11-10 23:30:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; 
 import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from 'components/GlobalStyle';
 import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -21,7 +20,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
            </PersistGate>
           </BrowserRouter>
          </ChakraProvider>
-      {/* <GlobalStyle/> */}
       </ThemeProvider>
       </Provider>
   </React.StrictMode>
